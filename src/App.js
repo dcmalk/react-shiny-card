517d1fa1ea3dd433144a121538c0550e41a3c643
Damon Malkiewicz <46790806+dcmalk@users.noreply.github.com>
2024-01-11 17:21:58 -0500

    Add background and card styles

--- src/App.js
+++ src/App.js
@@ -2,55 +2,68 @@ import React from 'react';
 import styled from 'styled-components';
 import coverImage from './assets/card-image.jpg';
 import avatarImage from './assets/card-avatar.jpg';
+import backgroundImage from './assets/card-background.jpg';
 
 // Styled components
 const Container = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   min-height: 100vh;
-  background-color: #121212; /* Dark background for the container */
-  color: #fff; /* Light text color */
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  color: #fff;
 `;
 
 const Card = styled.div`
-  width: 300px;
-  border: 1px solid #333; /* Darker border for the card */
-  border-radius: 8px;
-  overflow: hidden;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5); /* Adjusted shadow for dark mode */
-  background-color: #1e1e1e; /* Dark background for the card */
+  display: flex;
+  flex-direction: column;
+  width: 360px;
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+  box-shadow: 0px 30px 60px 0px rgba(0, 0, 0, 0.5), 0px 30px 60px 0px rgba(0, 0, 0, 0.1);
+  padding: 20px;
 `;
 
 const CardImage = styled.img`
   width: 100%;
-  height: 150px;
+  height: 300px;
   object-fit: cover;
+  border-radius: 10px;
 `;
 
 const CardContent = styled.div`
-  padding: 20px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
-  margin: 10px 0;
-  color: #fff; /* Light text color */
+  margin: 16px 0 10px 0;
+  color: #fff;
+  font-size: 17px;
+  font-weight: 600;
 `;
 
 const Divider = styled.hr`
-  border: 0;
+  border: none;
   height: 1px;
-  background: #333; /* Darker divider */
+  background: linear-gradient(90deg, rgba(255, 255, 255, 0.2) 0%, rgba(255, 255, 255, 0) 100%);
   margin: 10px 0;
 `;
 
 const Subtitle = styled.h3`
-  margin: 5px 0;
-  color: #ccc; /* Slightly lighter text color for contrast */
+  margin: 0 0 8px;
+  color: #ccc;
+  font-size: 15px;
+  font-weight: 500;
 `;
 
 const Text = styled.p`
-  color: #aaa; /* Lighter text color */
+  color: #aaa;
+  font-size: 13px;
+  line-height: 24px;
 `;
 
 const Author = styled.div`
@@ -67,8 +80,9 @@ const AuthorAvatar = styled.img`
 `;
 
 const AuthorName = styled.span`
-  font-weight: bold;
-  color: #fff; /* Light text color */
+  color: #fff;
+  font-size: 12px;
+  font-weight: 500;
 `;
 
 function App() {
